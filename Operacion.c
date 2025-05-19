e14e884d118d12d9fe0d712f7ca764f4fbfa291e
zei-ortega <A01663044@tec.mx>
2025-05-18 23:34:12 -0600

    Supervisor eliminó líneas

--- Operacion.c
+++ Operacion.c
@@ -5,10 +5,6 @@
 int main(){
     int num1 = 12;
     int num2 = 3;
-    int op1 = suma(num1,num2);
-    printf("%i",op1);
-    int op2 = resta(num1,num2);
-    printf("%i",op2);
     int op3 = multi(num1,num2);
     printf("%i",op3);
     return 0;
